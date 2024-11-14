11f0c75d42d17d4cbdd59550e2021071fda5adff
Ashish <ak503005@gmail.com>
2024-11-14 22:00:06 +0530

    Frontend Commits

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignupLogin from './components/SignupLogin';
+import ProductList from './components/ProductList';
+import ProductCreation from './components/ProductCreation';
+import ProductDetail from './components/ProductDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<SignupLogin />} />
+        <Route path='/cars' element={<ProductList />} />
+        <Route path='/add-car' element={<ProductCreation />} />
+        <Route path='/cars/:id' element={<ProductDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
